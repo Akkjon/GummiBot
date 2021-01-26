2a0c5bd71d1e7e498e4668ff42491845d9d058a9
Marcel <marcel.david.wagner@gmail.com>
2021-01-26 14:12:02 +0100

    added Locales

---
+++ Discord-Bot-MusicbotRemoveMessage/src/de/akkjon/pr/mbrm/Locales.java
@@ -0,0 +1,47 @@
+package de.akkjon.pr.mbrm;
+
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Locales {
+    private static Locale locale = Locale.getDefault();
+    private static final ResourceBundle messages = ResourceBundle.getBundle("MessageBundle", getLocale());
+
+    /**
+     * getter for {@link #locale}
+     *
+     * @return {@link Locale}
+     */
+    private static Locale getLocale() {
+        return locale;
+    }
+
+    /**
+     * setter for {@link #locale}
+     */
+    public static void setLocale(Locale input) {
+        locale = input;
+    }
+
+    /**
+     * gets a string from the {@link ResourceBundle} {@link #messages}
+     *
+     * @param key {@link String}
+     * @return {@link String}
+     */
+    public static String getString(String key) {
+        return messages.getString(key);
+    }
+
+    /**
+     * gets a string from the {@link ResourceBundle} {@link #messages}
+     *
+     * @param key    {@link String}
+     * @param params parameters for the placeholders in {@link String Strings} from the {@link ResourceBundle} {@link #messages}
+     * @return {@link String}
+     */
+    public static String getString(String key, Object... params) {
+        return MessageFormat.format(messages.getString(key), params);
+    }
+}
