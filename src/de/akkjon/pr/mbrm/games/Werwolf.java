cb7e5666f8d81ff4f3667595336168672d113044
Marcel <marcel.david.wagner@gmail.com>
2021-02-23 00:50:50 +0100

    erste Privatnachrichten und Callback

--- src/de/akkjon/pr/mbrm/games/Werwolf.java
+++ src/de/akkjon/pr/mbrm/games/Werwolf.java
@@ -3,71 +3,145 @@ package de.akkjon.pr.mbrm.games;
 import de.akkjon.pr.mbrm.Locales;
 import de.akkjon.pr.mbrm.Main;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
+import net.dv8tion.jda.api.entities.MessageHistory;
+import net.dv8tion.jda.api.entities.PrivateChannel;
+import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+interface WerwolfCallback {
+    void run(String answer);
+
+}
 
 public class Werwolf extends MultiPlayerGame {
 
     private int progress;
-    private Player[] players;
+    private final ArrayList<Player> playersArray = new ArrayList<>();
+
+    private final ArrayList<Player> hasReacted = new ArrayList<>();
 
     private interface Player {
-        long id = 0;
 
-        void nightAction();
+        void action();
 
-        void dayAction();
+        void sendMessage(long id, String message, WerwolfCallback callback);
     }
 
-    private class Dorfbewohner implements Player {
+    private class Everyone implements Player {
+        private final long id;
+
+        long getId() {
+            return this.id;
+        }
+
+        Everyone(long id) {
+            this.id = id;
+        }
 
         @Override
-        public void nightAction() {
+        public void action() {
 
         }
 
         @Override
-        public void dayAction() {
-
-        }
-    }
-
-    private class Amor extends Dorfbewohner implements Player {
-
-        @Override
-        public void nightAction() {
-
-        }
-
-        @Override
-        public void dayAction() {
+        public void sendMessage(long id, String message, WerwolfCallback callback) {
 
         }
     }
 
+    private class Dorfbewohner extends Everyone {
 
-    private static final String fileName = "ww";
+
+
+        Dorfbewohner(long id) {
+            super(id);
+        }
+
+        @Override
+        public void action() {
+            switch (progress) {
+                case 0:
+                    sendMessage(getId(), "Du kannst gar nix.", System.out::println);
+            }
+        }
+
+        @Override
+        public void sendMessage(long id, String message, WerwolfCallback callback) {
+            PrivateChannel channel = Objects.requireNonNull(Main.jda.getUserById(id)).openPrivateChannel().complete();
+            channel.sendMessage(message).queue();
+            callback.run("");
+        }
+    }
+
+    private class Amor extends Dorfbewohner {
+
+        Amor(long id) {
+            super(id);
+        }
+
+        @Override
+        public void action() {
+
+        }
+
+        @Override
+        public void sendMessage(long id, String message, WerwolfCallback callback) {
+            super.sendMessage(id, message, callback);
+        }
+    }
+
+    private class SuperWerwolf extends Everyone {
+
+        SuperWerwolf(long id) {
+            super(id);
+        }
+
+        @Override
+        public void action() {
+            switch (progress) {
+                case 0:
+                    sendMessage(getId(), "Wen willst du töten?", System.out::println);
+            }
+        }
+
+        public void sendMessage(long id, String message, WerwolfCallback callback) {
+            PrivateChannel channel = Objects.requireNonNull(Main.jda.getUserById(id)).openPrivateChannel().complete();
+            channel.sendMessage(message).queue();
+            Main.jda.addEventListener(new ListenerAdapter() {
+                @Override
+                public void onPrivateMessageReceived(@NotNull PrivateMessageReceivedEvent event) {
+                    Message answer = MessageHistory.getHistoryAround(event.getChannel(), event.getMessageId()).complete().getMessageById(event.getMessageId());
+                    if (answer != null) {
+                        callback.run(answer.getContentRaw());
+                    } else callback.run("HELP. ERROR");
+                }
+            });
+            callback.run("a");
+        }
+    }
 
     public Werwolf(long serverID) {
         super(serverID);
         createTextChannel(Locales.getString("msg.games.werwolf.channelName"));
-        sendStartMessage(fileName);
-        initReactionListeners();
+        sendStartMessage("werwolf");
     }
 
-    public void initReactionListeners() {
-        Main.jda.addEventListener(new ListenerAdapter() {
-            @Override
-            public void onGuildMessageReactionAdd(GuildMessageReactionAddEvent event) {
-                Message message = checkMessage(event);
-                if (message == null) return;
+    @Override
+    void startGame() {
+        for (long player : players) {
+            SuperWerwolf db = new SuperWerwolf(player);
+            playersArray.add(db);
+        }
 
-                if (event.getReactionEmote().getName().equals("➡")) {
-                    startGame();
-                } else if (event.getReactionEmote().getName().equals("❌")) {
-                    channel.delete().complete();
-                }
-            }
-        });
+        for (Player player : playersArray) {
+            player.action();
+        }
+
+        progress++;
+
     }
 }
