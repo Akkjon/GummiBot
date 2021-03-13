88354e25927e0724e3fdcb51c67d4dc3e154a847
Marcel <marcel.david.wagner@gmail.com>
2021-03-13 03:30:13 +0100

    random names, Abfrage über Rolle und paar neue Namen

--- src/de/akkjon/pr/mbrm/TalkChannels.java
+++ src/de/akkjon/pr/mbrm/TalkChannels.java
@@ -1,6 +1,11 @@
 package de.akkjon.pr.mbrm;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 import net.dv8tion.jda.api.entities.Category;
+import net.dv8tion.jda.api.entities.PermissionOverride;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
@@ -10,11 +15,16 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class TalkChannels {
+    static final Gson gson = new Gson();
     final long guildId;
     final Category category;
+    final List<String> names = getNames();
+
 
     public TalkChannels(long guildId) {
         this.guildId = guildId;
@@ -26,15 +36,15 @@ public class TalkChannels {
         Main.jda.addEventListener(new ListenerAdapter() {
             @Override
             public void onGuildVoiceJoin(@NotNull GuildVoiceJoinEvent event) {
-                if (amountOfEmptyChannels() == 0) {
-                    createNewChannel("Test1");
+                if (getEmptyChannels().size() == 0) {
+                    createNewChannel();
                 }
             }
 
             @Override
             public void onGuildVoiceMove(@NotNull GuildVoiceMoveEvent event) {
-                if (amountOfEmptyChannels() == 0)
-                    createNewChannel("Test1");
+                if (getEmptyChannels().size() == 0)
+                    createNewChannel();
                 else removeUnnecessaryChannels();
 
             }
@@ -47,9 +57,10 @@ public class TalkChannels {
     }
 
     private void removeUnnecessaryChannels() {
-        if (amountOfEmptyChannels() >= 2) {
+        List<VoiceChannel> channels = getEmptyChannels();
+        if (channels.size() >= 2) {
             VoiceChannel firstEmpty = null;
-            for (VoiceChannel c : category.getVoiceChannels()) {
+            for (VoiceChannel c : channels) {
                 if (c.getMembers().isEmpty()) {
                     if (firstEmpty == null) {
                         firstEmpty = c;
@@ -61,18 +72,34 @@ public class TalkChannels {
         }
     }
 
-    void createNewChannel(String name) {
-        category.createVoiceChannel(name).queue();
+    static List<String> getNames() {
+        JsonArray array = gson.fromJson(Storage.getInternalFile("channelNames.json"), JsonObject.class).get("names").getAsJsonArray();
+
+        return gson.fromJson(array, new TypeToken<List<String>>() {
+        }.getType());
     }
 
-    private int amountOfEmptyChannels() {
-        int result = 0;
+    void createNewChannel() {
+        category.createVoiceChannel(names.get((int) (Math.random() * names.size()))).queue();
+    }
+
+    private List<VoiceChannel> getEmptyChannels() {
+        List<VoiceChannel> channels = new ArrayList<>();
+
         for (VoiceChannel c : category.getVoiceChannels()) {
             if (c.getMembers().isEmpty()) {
-                result++;
+                boolean perm = false;
+                for (PermissionOverride p : c.getPermissionOverrides()) {
+                    if (p.isRoleOverride()) {
+                        if (p.getRole().getName().equals("Perm")) {
+                            perm = true;
+                        }
+                    }
+                }
+                if (!perm) channels.add(c);
             }
         }
-        return result;
+        return channels;
     }
 
     public Category getOrCreateCategory() {
@@ -82,12 +109,10 @@ public class TalkChannels {
                     Storage.rootFolder + guildId + File.separator + "talkCategory.txt", null);
             if (id != null && !id.isBlank()) cat = Main.jda.getCategoryById(id);
         } catch (IOException ignored) {
-            cat = Objects.requireNonNull(Main.jda.getGuildById(guildId)).createCategory("Talks").complete();
-            cat.createVoiceChannel("Talk 1").queue();
         }
         if (cat == null) {
             cat = Objects.requireNonNull(Main.jda.getGuildById(guildId)).createCategory("Talks").complete();
-            cat.createVoiceChannel("Talk 1").queue();
+            cat.createVoiceChannel(names.get((int) (Math.random() * names.size()))).queue();
             try {
                 Storage.saveFile(Storage.rootFolder + guildId + File.separator + "talkCategory.txt", cat.getId());
             } catch (IOException e) {
